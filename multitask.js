f6bf84c216dcfb411ec832a214e167db1f1a308d
Ishara <newair007@gmail.com>
2023-07-05 12:14:42 +0930

    add comment

--- multitask.js
+++ multitask.js
@@ -3,6 +3,29 @@ const https = require('https');
 const crypto = require('crypto');
 const fs = require('fs');
 
+process.env.UV_THREADPOOL_SIZE=5;
+
+// If you set the above value to 4 the output is as follows
+// Request: 883
+// Hash: 8542
+// Read file: 8544
+// Hash: 8684
+// Hash: 8805
+// Hash: 9025
+
+// Which means it it allocates four threads but since read file is an IO operation
+// it releases that thread and allocates that threadto calculate Hash. But when one of threads which runs the
+// hash functions finishes up calculating it allocates the thread back to read the file.But
+
+// This can be clearly seen when you increase the UV_THREADPOOL_SIZE defined above where the output becomes follows
+
+// Request: 883
+// Hash: 8542
+// Read file: 8544
+// Hash: 8684
+// Hash: 8805
+// Hash: 9025
+
 const start = Date.now();
 function doRequest() {
 
